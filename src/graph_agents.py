f8bb98304d737421c90fdd12ae6126ff00e7bfb2
kesamet <kesamet@gmail.com>
2024-03-19 16:50:58 +0800

    Update

---
+++ src/graph_agents.py
@@ -0,0 +1,125 @@
+import json
+import operator
+from typing import Annotated, Sequence, TypedDict
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage, FunctionMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolInvocation, ToolExecutor
+
+tools = [TavilySearchResults(max_results=1)]
+tool_executor = ToolExecutor(tools)
+
+model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0, streaming=False)
+model_with_tools = model.bind(functions=tools)
+
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+
+def should_continue(state):
+    """Function that determines whether to continue or not."""
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no function call, then we finish
+    if "function_call" not in last_message.additional_kwargs:
+        return "end"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
+
+
+def call_model(state):
+    """Function that calls the model."""
+    messages = state["messages"]
+    response = model_with_tools.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+def call_tool(state):
+    """Function to execute tools."""
+    messages = state["messages"]
+    # Based on the continue condition
+    # we know the last message involves a function call
+    last_message = messages[-1]
+    # We construct an ToolInvocation from the function_call
+    action = ToolInvocation(
+        tool=last_message.additional_kwargs["function_call"]["name"],
+        tool_input=json.loads(
+            last_message.additional_kwargs["function_call"]["arguments"]
+        ),
+    )
+    # We call the tool_executor and get back a response
+    response = tool_executor.invoke(action)
+    # We use the response to create a FunctionMessage
+    function_message = FunctionMessage(content=str(response), name=action.tool)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [function_message]}
+
+
+def build_graph():
+    # Define a new graph
+    workflow = StateGraph(AgentState)
+
+    # Define the two nodes we will cycle between
+    workflow.add_node("agent", call_model)
+    workflow.add_node("action", call_tool)
+
+    # Set the entrypoint as `agent`
+    # This means that this node is the first one called
+    workflow.set_entry_point("agent")
+
+    # We now add a conditional edge
+    workflow.add_conditional_edges(
+        # First, we define the start node. We use `agent`.
+        # This means these are the edges taken after the `agent` node is called.
+        "agent",
+        # Next, we pass in the function that will determine which node is called next.
+        should_continue,
+        # Finally we pass in a mapping.
+        # The keys are strings, and the values are other nodes.
+        # END is a special node marking that the graph should finish.
+        # What will happen is we will call `should_continue`, and then the output of that
+        # will be matched against the keys in this mapping.
+        # Based on which one it matches, that node will then be called.
+        {
+            # If `tools`, then we call the tool node.
+            "continue": "action",
+            # Otherwise we finish.
+            "end": END,
+        },
+    )
+
+    # We now add a normal edge from `tools` to `agent`.
+    # This means that after `tools` is called, `agent` node is called next.
+    workflow.add_edge("action", "agent")
+
+    # Finally, we compile it!
+    # This compiles it into a LangChain Runnable,
+    # meaning you can use it as you would any other runnable
+    app = workflow.compile()
+    return app
+
+
+if __name__ == "__main__":
+    app = build_graph()
+
+    from langchain_core.messages import HumanMessage
+
+    inputs = {"messages": [HumanMessage(content="what is the weather in sf")]}
+
+    # Sample run
+    res = app.invoke(inputs)
+    print(res["messages"][-1].content)
+
+    # Streaming node output
+    for output in app.stream(inputs):
+        # stream() yields dictionaries with output keyed by node name
+        for key, value in output.items():
+            print(f"Output from node '{key}':")
+            print("---")
+            print(value)
+        print("\n---\n")
