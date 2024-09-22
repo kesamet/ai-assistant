176cbc1704a9b1ccb1d6e24254561f0575e8988f
kesamet <kesamet@gmail.com>
2024-09-22 21:01:01 +0800

    Update

--- src/general.py
+++ src/general.py
@@ -10,35 +10,6 @@ from loguru import logger
 from PIL import Image
 
 
-def encode_image_from_uri(uri: str) -> str:
-    """Get base64 encoded string from image URI."""
-    with open(uri, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
-
-
-def encode_image_from_pil(image: Image) -> str:
-    """Encode a PIL image to base64 encoded string."""
-    buffered = BytesIO()
-    image.save(buffered, format="png")
-    base64_bytes = base64.b64encode(buffered.getvalue())
-    return base64_bytes.decode("utf-8")
-
-
-def encode_page(page: fitz.Page) -> str:
-    """Encode a fitz page to base64 encoded string."""
-    pix = page.get_pixmap()
-    return base64.b64encode(pix.pil_tobytes("png")).decode("utf-8")
-
-
-def decode_image(field: str) -> np.ndarray:
-    """Decode a base64 encoded image to a numpy array of floats."""
-    import cv2
-
-    array = np.frombuffer(base64.b64decode(field), dtype=np.uint8)
-    image_array = cv2.imdecode(array, cv2.IMREAD_ANYCOLOR)  # BGR
-    return image_array
-
-
 def perform(func, filebytes, **kwargs):
     """Wrapper function to perform func for bytes file."""
     fh, temp_filename = tempfile.mkstemp()
@@ -69,3 +40,32 @@ def sleep(timeout, retry=3):
         return wrapper
 
     return the_real_decorator
+
+
+def encode_image_from_uri(uri: str) -> str:
+    """Get base64 encoded string from image URI."""
+    with open(uri, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def encode_image_from_pil(image: Image.Image) -> str:
+    """Encode a PIL image to base64 encoded string."""
+    buffered = BytesIO()
+    image.save(buffered, format="png")
+    base64_bytes = base64.b64encode(buffered.getvalue())
+    return base64_bytes.decode("utf-8")
+
+
+def encode_page(page: fitz.Page) -> str:
+    """Encode a fitz page to base64 encoded string."""
+    pix = page.get_pixmap()
+    return base64.b64encode(pix.pil_tobytes("png")).decode("utf-8")
+
+
+def decode_image(field: str) -> np.ndarray:
+    """Decode a base64 encoded image to a numpy array of floats."""
+    import cv2
+
+    array = np.frombuffer(base64.b64decode(field), dtype=np.uint8)
+    image_array = cv2.imdecode(array, cv2.IMREAD_ANYCOLOR)  # BGR
+    return image_array
